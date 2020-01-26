2f6401a35013c070d4e138b068bb7a8773f8973b
AndrewLymar <andriilymar@gmail.com>
2020-01-26 17:01:28 +0200

    Up

--- app/js/common.js
+++ app/js/common.js
@@ -1,16 +1,40 @@
- $(function () {
-     $('nav').mobileMenu({
-         menuIconClassName: ".menu-icon",
-         mobileResolution: 768,
-         menuType: "sticky",
-         offsetToSticky: 50,
-         closeIconClassName: ".close-menu-icon"
-     });
+var hellopreloader = document.getElementById("hellopreloader_preload");
 
-     var gallery = $('.about__img-gallery a').simpleLightbox();
+function fadeOutnojquery(el) {
+    el.style.opacity = 1;
 
-     $(".version").hide();
-     $(".version-button").click(function () {
-         $(this).next().slideToggle();
-     });
- });
+    var interhellopreloader = setInterval(function () {
+            el.style.opacity = el.style.opacity - 0.05;
+            if (el.style.opacity <= 0.05) {
+                clearInterval(interhellopreloader);
+                hellopreloader.style.display = "none";
+            }
+        }
+
+        , 16);
+}
+
+window.onload = function () {
+    setTimeout(function () {
+            fadeOutnojquery(hellopreloader);
+        }
+
+        , 1000);
+};
+
+$(function () {
+    $('nav').mobileMenu({
+        menuIconClassName: ".menu-icon",
+        mobileResolution: 768,
+        menuType: "sticky",
+        offsetToSticky: 50,
+        closeIconClassName: ".close-menu-icon"
+    });
+
+    var gallery = $('.about__img-gallery a').simpleLightbox();
+
+    $(".version").hide();
+    $(".version-button").click(function () {
+        $(this).next().slideToggle();
+    });
+});
